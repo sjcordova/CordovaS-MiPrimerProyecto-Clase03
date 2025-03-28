f47bb1518b93b3381ef31e521f90cb3fb9b2f59c
laboratorio <laboratorio@UPOAULA107>
2025-03-28 07:24:10 -0500

    Agregar archivos de proyecto.

---
+++ CordovaS-MiPrimerProyecto-Clase02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CordovaS_MiPrimerProyecto_Clase02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
